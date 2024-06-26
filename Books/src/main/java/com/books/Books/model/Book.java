f91a6a4b21c3cd8c894ca7c10bc4ccf862973aac
deliaanggun <121421727+deliaanggun@users.noreply.github.com>
2024-06-26 22:33:12 +0700

    Add files via upload

---
+++ Books/src/main/java/com/books/Books/model/Book.java
@@ -0,0 +1,22 @@
+package com.books.Books.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="Book")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String author;
+}
---
+++ Books/src/main/java/com/books/Books/repo/BookRepo.java
@@ -0,0 +1,9 @@
+package com.books.Books.repo;
+
+import com.books.Books.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepo extends JpaRepository<Book, Long> {
+}
